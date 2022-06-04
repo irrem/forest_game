867ebcc4eb9a617d5c340cb2bb52f5d838c4bf80
Rukiye Irem Sarisoy <srsyirm@hotmail.com>
2022-06-04 15:30:16 +0300

    progress bar added

--- Assets/Scripts/GeneralManager.cs
+++ Assets/Scripts/GeneralManager.cs
@@ -1,15 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class GeneralManager : MonoBehaviour
 {
     public GameObject people;
     public bool is_people=false;
+    public GameObject levelBar;
+    public int time;
+    public float levelScore;
 
     // Start is called before the first frame update
     void Start()
-    {
+    { 
+        levelScore = 0;
         //InvokeRepeating("people_coming", 0.0f, 1.0f);
         Invoke("people_coming", 2.0f);
 
@@ -17,6 +18,7 @@ public class GeneralManager : MonoBehaviour
     }
     void people_coming()
     {
+       
         float rand = Random.Range(-5.0f, 12.0f);
         GameObject new_people = Instantiate(people, new Vector3(-8, 0.8f, rand),Quaternion.identity);
         Debug.Log("people is coming");
@@ -25,6 +27,16 @@ public class GeneralManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        if (levelScore / 100 >= 1)
+        {
+            Debug.Log("Congragulations!");
+        }
+        else
+        {
+            levelScore += 0.1f;
+            levelBar.transform.localScale = new Vector3(levelScore / 100, 1, 1);
+        }
         if (is_people)
         {
             Vector3 position = people.transform.position;
@@ -34,9 +46,9 @@ public class GeneralManager : MonoBehaviour
                 Destroy(people);
             }
             people.transform.position = position;
-           
-
 
         }
+
     }
+   
 }
